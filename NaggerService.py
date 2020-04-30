d599ded54270cd8c59e1ee80e858dd8aaba1882d
EthanShiloh <et.shiloh@gmail.com>
2020-04-30 20:54:59 +0300

    Added service operation. currently not working

--- NaggerService.py
+++ NaggerService.py
@@ -2,6 +2,26 @@ import time
 import random
 from pathlib import Path
 from SMWinservice import SMWinservice
+from DuolingoNagger import DuolingoNagger, ret_duolingo_obj
+import argparse
+
+
+NAG_INTERVAL_SEC = 10
+
+def arg_parse_nagger():
+    parser = argparse.ArgumentParser(description = '')
+    parser.add_argument('-n', '--nagger', dest='nagger', help='Wanted nagger. Options are: duolingo', required=True)
+
+    return parser.parse_known_args()
+
+def get_conf_nagger():
+    args = arg_parse_nagger()[0] # parse_known_args returns a tuple, with all the args not recognized by the current argparse
+    switcher = {
+        'duolingo': ret_duolingo_obj,
+    }
+    func = switcher.get(args.nagger, "Invalid nagger")
+    return func()
+    
 
 class PythonCornerExample(SMWinservice):
     _svc_name_ = "PythonCornerExample"
@@ -9,6 +29,7 @@ class PythonCornerExample(SMWinservice):
     _svc_description_ = "That's a great winservice! :)"
 
     def start(self):
+        self.nagger = get_conf_nagger()
         self.isrunning = True
 
     def stop(self):
@@ -16,4 +37,8 @@ class PythonCornerExample(SMWinservice):
 
     def main(self):
         pass
-
+"""         while(True):
+            if (self.nagger.should_nag()):
+                self.nagger.nag()
+                time.sleep(NAG_INTERVAL_SEC)
+ """
